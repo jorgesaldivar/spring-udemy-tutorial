cd80eae575519df8786e11eafb769168121291e0
jorge.saldivar <jorge.saldivar@MW78L2QZ5PSI5R.dir.svc.accenture.com>
2015-11-04 11:53:52 -0600

    update

--- helloword/src/main/java/com/spring/test/Person.java
+++ helloword/src/main/java/com/spring/test/Person.java
@@ -2,6 +2,30 @@ package com.spring.test;
 
 public class Person {
 
+	private int id;
+	private String name;
+	
+	public Person() {
+		//super();
+		//this.id = id;
+	//	this.name = name;
+	}
+	
+	public Person(int id, String name) {
+		//super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + "]";
+	}
+
+
+	
+
 
 	public void speak() {
 		System.out.println("Hello I'm a person");
