46f8dbee7cc867e26d2d064131c191ea49c57022
Jorge Saldivar <1724724+jorgesaldivar@users.noreply.github.com>
2015-12-13 01:19:31 -0600

    45

--- spring-tutorial-37/src/main/java/com/caveofprogramming/spring/test/App.java
+++ spring-tutorial-37/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -1,19 +1,36 @@
 package com.caveofprogramming.spring.test;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
 
 public class App {
 
 	public static void main(String[] args) {
-		
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
-		
-		Robot robot = (Robot)context.getBean("robot");
-		
-		robot.speak();
-		
-		((ClassPathXmlApplicationContext)context).close();
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/caveofprogramming/spring/test/beans/beans.xml");
+
+		// Robot robot = (Robot)context.getBean("robot");
+
+		// robot.speak();
+
+		OffersDAO offersDao = (OffersDAO) context.getBean("offersDao");
+
+		try {
+			List<Offer> offers = offersDao.getOffers();
+
+			for (Offer offer : offers) {
+				System.out.println(offer);
+			}
+		} catch (DataAccessException ex) {
+			System.out.println(ex.getMessage());
+			System.out.println(ex.getClass());
+		}
+
+		((ClassPathXmlApplicationContext) context).close();
 	}
 
 }
