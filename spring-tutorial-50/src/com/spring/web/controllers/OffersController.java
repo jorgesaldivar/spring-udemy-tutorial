fbf0e6089afaddaeb61500c927f839f4f9fd466e
Jorge Saldivar <1724724+jorgesaldivar@users.noreply.github.com>
2016-02-29 23:36:55 -0600

    mvc

---
+++ spring-tutorial-50/src/com/spring/web/controllers/OffersController.java
@@ -0,0 +1,37 @@
+package com.spring.web.controllers;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class OffersController {
+
+	/*@RequestMapping("/")
+	public String showHome(HttpSession session) {
+		session.setAttribute("name", "barris");
+		return "home";
+	}*/
+	
+	/*@RequestMapping("/")
+	public ModelAndView showHome() {
+		
+		ModelAndView mv = new ModelAndView("home");
+		Map<String, Object> model = mv.getModel();
+		model.put("name", "barris");
+		return mv;
+	}*/
+
+	@RequestMapping("/")
+	public String showHome(Model model) {
+		model.addAttribute("name","model");
+		return "home";
+	}
+	
+}
+
